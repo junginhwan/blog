d98c3837011e648b1e78d5a5871fc7e54a613cc8
vagrant <vagrant@homestead>
2021-01-16 23:12:16 +0000

    카테고리, 콜렉션 데이터 모델링

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $categories = [
+            'posts',
+            'collections'
+        ];
+        foreach ($categories as $category) {
+            Category::create([
+                'name' => $category
+            ]);
+        }
+    }
+}
